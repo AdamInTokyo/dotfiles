8fec1b3a9fd16cac848e95bc8d1eebdf4ada2e63
AdamInTokyo <adam.strange.phd@gmail.com>
2024-11-15 18:27:39 +0900

    Added bespoke directory with files for non-lazyVim lazy.nvim config

---
+++ nvim/bespoke/conform.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  require("conform").setup({
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- You can customize some of the format options for the filetype (:help conform.format)
+      rust = { "rustfmt", lsp_format = "fallback" },
+      -- Conform will run the first available formatter
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  }),
+}
