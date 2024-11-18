1bfe7198b0ee507c09be5bd6c6990571bfe34a42
AdamInTokyo <adam.strange.phd@gmail.com>
2024-11-18 18:29:05 +0900

    Edited return table for mason to account for multiple repositories in one file.

--- nvim/bespoke/mason.lua
+++ nvim/bespoke/mason.lua
@@ -1,41 +1,48 @@
 return {
 
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  cmd = "Mason",
-  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-  build = ":MasonUpdate",
-  opts_extend = { "ensure_installed" },
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shfmt",
+  {
+    "williamboman/mason.nvim",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "basedpyright",
+        "ruff",
+      },
     },
-  },
-  ---@param opts MasonSettings | {ensure_installed: string[]}
-  config = function(_, opts)
-    require("mason").setup(opts)
-    local mr = require("mason-registry")
-    mr:on("package:install:success", function()
-      vim.defer_fn(function()
-        -- trigger FileType event to possibly load this newly installed LSP server
-        require("lazy.core.handler.event").trigger({
-          event = "FileType",
-          buf = vim.api.nvim_get_current_buf(),
-        })
-      end, 100)
-    end)
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require("lazy.core.handler.event").trigger({
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
 
-    mr.refresh(function()
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
+      mr.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
         end
-      end
-    end)
-  end,
-
-  require("mason-lspconfig").setup(), --TODO: Is it possible to use this inside a return block?
+      end)
+      -- mason's lspconfig needs to be setup after the regular mason setup
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    require("mason-lspconfig").setup(),
+  },
 }
