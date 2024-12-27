e0a0d7f249c533f5dfa30dac380875ccf60fb87f
Adam Strange <adam.strange.phd@gmail.com>
2024-12-27 17:46:37 +0900

    Added lsp keybinds, diagnostic icons to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -51,12 +51,14 @@ require("lint").linters_by_ft = {
 -- Key binds
 vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, { noremap = true, silent = true, desc = "LSP Formatter" })
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true, desc = "Default LSP Hover" })
+vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, { noremap = true, silent = true, desc = "LSP Signature Help" })
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true, desc = "Go to definition" })
+vim.keymap.set('n', '<leader>uh', '<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>', { noremap = true, silent = true, desc = "Show inlay hints" })
 -- below are from https://wagomu.me/blog/2023-05-17-vim-ekiden
-vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+-- vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+-- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
 vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
 vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
@@ -86,3 +88,19 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+-- Gutter Icons and highlights
+vim.diagnostic.config({
+   signs = {
+      text = {
+         [vim.diagnostic.severity.ERROR] = '󰈸',
+         [vim.diagnostic.severity.WARN] = '',
+         [vim.diagnostic.severity.HINT] = '',
+      },
+      linehl = {
+      },
+      numhl = {
+         -- [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+         -- [vim.diagnostic.severity.WARN] = 'WarningMsg',
+      },
+   },
+})
