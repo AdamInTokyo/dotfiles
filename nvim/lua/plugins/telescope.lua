05122085edfcf4318629f28f2a7f3004e7df74cb
AdamInTokyo <adam.strange.phd@gmail.com>
2025-08-12 14:26:57 +0900

    Set telescope preview pane layout to vertical to accomodate cramped monitors

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,18 @@
 return {
-   'nvim-telescope/telescope.nvim',
-   tag = '0.1.8',
-   dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('telescope').setup {
+            defaults = {
+                layout_strategy = 'vertical',
+                layout_config = {
+                    height = 0.9,
+                    preview_cutoff = 40,
+                    prompt_position = "bottom",
+                    width = 0.8
+                },
+            },
+        }
+    end
 }
