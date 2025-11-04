26d39f063e6e7ab67626d859950660a524c0fde2
AdamInTokyo <adam.strange.phd@gmail.com>
2025-11-05 01:10:10 +0900

    Added 'dd' bind to normal mode in telescope buffer picker to delete the currently selected buffer.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,18 +1,27 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require('telescope').setup {
-            defaults = {
-                layout_strategy = 'vertical',
-                layout_config = {
-                    height = 0.9,
-                    preview_cutoff = 40,
-                    prompt_position = "bottom",
-                    width = 0.8
-                },
+   'nvim-telescope/telescope.nvim',
+   tag = '0.1.8',
+   dependencies = { 'nvim-lua/plenary.nvim' },
+   config = function()
+      require('telescope').setup {
+         defaults = {
+            layout_strategy = 'vertical',
+            layout_config = {
+               height = 0.9,
+               preview_cutoff = 40,
+               prompt_position = "bottom",
+               width = 0.8
             },
-        }
-    end
+         },
+         pickers = {
+            buffers = {
+               mappings = {
+                  n = {
+                     ["dd"] = "delete_buffer",
+                  }
+               }
+            }
+         }
+      }
+   end
 }
