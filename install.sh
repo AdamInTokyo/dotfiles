7cd26bea13a2779caeacc16196bb0b2a4ff49594
AdamInTokyo <adam.strange.phd@gmail.com>
2026-04-14 23:03:14 +0900

    Re-added tree-sitter-cli install.

--- install.sh
+++ install.sh
@@ -24,8 +24,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 . "$HOME/.cargo/env"
 
 # Install tree-sitter
-#apt-get -y install libclang-dev
-#cargo install --locked tree-sitter-cli
+apt-get -y install libclang-dev
+cargo install --locked tree-sitter-cli
 
 # Install yazi
 cargo install --force yazi-build
