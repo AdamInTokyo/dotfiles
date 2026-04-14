ca7194777e8e99628db2226adfccf10f626a9896
AdamInTokyo <adam.strange.phd@gmail.com>
2026-04-14 16:51:58 +0900

    Updated treesitter to main branch for 0.12+ compatibility.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,19 +4,10 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+    branch = "main", -- master branch is deprecated, use main for nvim 0.12+
     build = ":TSUpdate",
     event = "VeryLazy",
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
+    lazy = false,
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     keys = {
       { "<c-space>", desc = "Increment Selection" },
@@ -83,37 +74,55 @@ return {
     ---end,
   },
 
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    event = "VeryLazy",
-    enabled = true,
-    config = function()
-      -- If treesitter is already loaded, we need to run config again for textobjects
-      -- if LazyVim.is_loaded("nvim-treesitter") then
-      --   local opts = LazyVim.opts("nvim-treesitter")
-      --   require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
-      -- end
+--   {
+--     "nvim-treesitter/nvim-treesitter-textobjects",
+--     event = "VeryLazy",
+--     enabled = true,
+--     config = function()
+--       -- If treesitter is already loaded, we need to run config again for textobjects
+--       -- if LazyVim.is_loaded("nvim-treesitter") then
+--       --   local opts = LazyVim.opts("nvim-treesitter")
+--       --   require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
+--       -- end
+--
+--       -- When in diff mode, we want to use the default
+--       -- vim text objects c & C instead of the treesitter ones.
+--       local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+--       local configs = require("nvim-treesitter.configs")
+--       for name, fn in pairs(move) do
+--         if name:find("goto") == 1 then
+--           move[name] = function(q, ...)
+--             if vim.wo.diff then
+--               local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+--               for key, query in pairs(config or {}) do
+--                 if q == query and key:find("[%]%[][cC]") then
+--                   vim.cmd("normal! " .. key)
+--                   return
+--                 end
+--               end
+--             end
+--             return fn(q, ...)
+--           end
+--         end
+--       end
+--     end,
+--   },
+   {
+     "nvim-treesitter/nvim-treesitter-textobjects",
+     branch = "main",
+     init = function()
+       -- Disable entire built-in ftplugin mappings to avoid conflicts.
+       -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+       vim.g.no_plugin_maps = true
 
-      -- When in diff mode, we want to use the default
-      -- vim text objects c & C instead of the treesitter ones.
-      local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-      local configs = require("nvim-treesitter.configs")
-      for name, fn in pairs(move) do
-        if name:find("goto") == 1 then
-          move[name] = function(q, ...)
-            if vim.wo.diff then
-              local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-              for key, query in pairs(config or {}) do
-                if q == query and key:find("[%]%[][cC]") then
-                  vim.cmd("normal! " .. key)
-                  return
-                end
-              end
-            end
-            return fn(q, ...)
-          end
-        end
-      end
-    end,
-  },
+       -- Or, disable per filetype (add as you like)
+       -- vim.g.no_python_maps = true
+       -- vim.g.no_ruby_maps = true
+       -- vim.g.no_rust_maps = true
+       -- vim.g.no_go_maps = true
+     end,
+     config = function()
+       -- put your config here
+     end,
+   },
 }
