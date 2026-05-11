d0909b04a752ca5af413257c30fac53be852a227
AdamInTokyo <adam.strange.phd@gmail.com>
2026-05-11 18:21:02 +0900

    Added tabs and a toggle tabs bind (ctrl+;) to wezterm config.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,6 +3,7 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
+config.term = 'wezterm'
 
 -- Set to launch with WSL2
 config.default_domain = 'WSL:Ubuntu'
@@ -11,8 +12,10 @@ config.color_scheme_dirs = { "C:\\Users\\modal\\zenbones\\wezterm\\zenburned.tom
 config.color_scheme = 'zenburned'
 
 config.font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'Medium' })
+config.font_size = 11.0
 
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = true
 
 config.audible_bell = "Disabled"
 
@@ -23,7 +26,8 @@ config.default_cursor_style = "BlinkingBar"
 config.initial_cols = 140
 config.initial_rows = 60
 
-config.window_decorations = "RESIZE|TITLE"
+-- config.window_decorations = "RESIZE|TITLE"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 config.window_padding = {
   left = '2cell',
@@ -32,11 +36,50 @@ config.window_padding = {
   bottom = '1cell',
 }
 
+config.window_frame = {
+  font = wezterm.font { family = 'Roboto' },
+  font_size = 10.0,
+}
+
+
+config.colors = {
+  compose_cursor = '#f9f9f9',
+  tab_bar = {
+    active_tab = {
+      -- fg_color = "#f0e4cf",
+      fg_color = "white",
+      bg_color = "#404040",
+      underline = "Double",
+      intensity = "Bold",
+    }
+  }
+}
+
+
+
 config.force_reverse_video_cursor = true
 
 config.ime_preedit_rendering = 'Builtin' -- other option is System
-config.colors = {
-	compose_cursor = '#f9f9f9',
+-- config.colors = {
+-- 	compose_cursor = '#f9f9f9',
+-- }
+
+config.scrollback_lines = 10000
+
+wezterm.on("toggle-tabbar", function(window, _)
+	local overrides = window:get_config_overrides() or {}
+	if overrides.enable_tab_bar == false then
+		wezterm.log_info("tab bar shown")
+		overrides.enable_tab_bar = true
+	else
+		wezterm.log_info("tab bar hidden")
+		overrides.enable_tab_bar = false
+	end
+	window:set_config_overrides(overrides)
+end)
+
+config.keys = {
+  { key = ';', mods = 'CTRL', action = wezterm.action.EmitEvent 'toggle-tabbar' },
 }
 
 return config
